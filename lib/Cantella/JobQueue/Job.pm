79baee6b59477e19077e0461644d70a971edf2e0
Guillermo Roditi <groditi@gmail.com>
2009-12-01 14:35:02 -0500

    job object and store API docs

---
+++ lib/Cantella/JobQueue/Job.pm
@@ -0,0 +1,43 @@
+package Cantella::JobQueue::Job;
+
+use Moose;
+
+has id => (
+  is => 'rw',
+  isa => 'Defined',
+  predicate => 'has_id',
+);
+
+has priority => (
+  is => 'rw',
+  isa => 'Int',
+  predicate => 'has_priority',
+);
+
+has job_type => (
+  is => 'rw',
+  isa => 'Str',
+  required => 1,
+);
+
+has arguments => (
+  is => 'rw',
+  isa => 'Value',
+  required => 1
+);
+
+has delay_until => (
+  is => 'rw',
+  isa => 'DateTime',
+  predicate => 'has_delay_until',
+);
+
+has completed_successfully => (
+  is => 'rw',
+  isa => 'Bool',
+  predicate => 'is_completed',
+);
+
+1;
+
+__END__;
